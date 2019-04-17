25b20534d80d09500cd2d1fe9cb7440002915e04
Meredith Xu <meredithxu@169-231-84-3.wireless.ucsb.edu>
2019-04-16 17:42:03 -0700

    something is wrong

--- build_simple_example.py
+++ build_simple_example.py
@@ -4,18 +4,19 @@ with open("four_blocks.txt",'w') as file:
   writer = csv.writer(file, delimiter = ",")
   writer.writerow(["ts", "user" ,"x_coordinate" ,"y_coordinate" ,"color", "label"])
 
-  for i in range(0,500):
-    for j in range(0, 500):
-        writer.writerow([0, 0, i, j, 0, 4])
+  for i in range(0,50):
+    for j in range(0, 50):
+        writer.writerow([0, 0, i, j, 0, 0])
 
-  for i in range(500,1000):
-    for j in range(0, 500):
-        writer.writerow([0, 0, i, j, 1, 5])
+  for i in range(50,100):
+    for j in range(0, 50):
+        writer.writerow([0, 0, i, j, 1, 1])
+'''
+  for i in range(0,5):
+    for j in range(5, 10):
+        writer.writerow([0, 0, i, j, 2, 2])
 
-  for i in range(0,500):
-    for j in range(500, 1000):
-        writer.writerow([0, 0, i, j, 2, 6])
-
-  for i in range(500,1000):
-    for j in range(500, 1000):
-        writer.writerow([0, 0, i, j, 3, 7])
+  for i in range(5,10):
+    for j in range(5, 10):
+        writer.writerow([0, 0, i, j, 3, 3])
+'''
