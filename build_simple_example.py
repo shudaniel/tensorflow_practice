0f7d01b0c86b0cb8ead04ca1e2023dc959a73938
Daniel Shu <danielshu98@gmail.com>
2019-04-24 17:08:33 -0700

    began documenting

--- build_simple_example.py
+++ build_simple_example.py
@@ -1,6 +1,6 @@
 import csv
 
-with open("four_blocks_lr.txt",'w') as file:
+with open("two_blocks_lr.txt",'w') as file:
   writer = csv.writer(file, delimiter = ",")
   writer.writerow(["ts", "user" ,"x_coordinate" ,"y_coordinate" ,"color", "label"])
 
@@ -13,21 +13,8 @@ with open("four_blocks_lr.txt",'w') as file:
     for j in range(0, 100):
         writer.writerow([0, 0, i, j, 1, 1])
 
-# with open("four_blocks_rl.txt",'w') as file:
-#   writer = csv.writer(file, delimiter = ",")
-#   writer.writerow(["ts", "user" ,"x_coordinate" ,"y_coordinate" ,"color", "label"])
 
-#   # Set 2: Color 1 on right, color 0 on left
-#   for i in range(0,50):
-#     for j in range(0, 100):
-#         writer.writerow([0, 0, i, j, 1, 1])
-
-#   for i in range(50,100):
-#     for j in range(0, 100):
-#         writer.writerow([0, 0, i, j, 0, 0])
-
-
-with open("four_blocks_diag.txt",'w') as file:
+with open("two_blocks_diag.txt",'w') as file:
   writer = csv.writer(file, delimiter = ",")
   writer.writerow(["ts", "user" ,"x_coordinate" ,"y_coordinate" ,"color", "label"])
 
@@ -41,16 +28,7 @@ with open("four_blocks_diag.txt",'w') as file:
         writer.writerow([0, 0, i, j, 1, 1])
 
 
-  # # Set 4: color 1 on upper right triangle, color 0 on lower left triangle
-  # for i in range(0,100):
-  #   for j in range(i, 100):
-  #       writer.writerow([0, 0, i, j, 1, 1])
-
-  # for j in range(0,100):
-  #   for i in range(0, j):
-  #       writer.writerow([0, 0, i, j, 0, 0])
-
-with open("four_blocks_tb.txt",'w') as file:
+with open("two_blocks_tb.txt",'w') as file:
   writer = csv.writer(file, delimiter = ",")
   writer.writerow(["ts", "user" ,"x_coordinate" ,"y_coordinate" ,"color", "label"])
 
@@ -63,15 +41,6 @@ with open("four_blocks_tb.txt",'w') as file:
     for j in range(50, 100):
         writer.writerow([0, 0, i, j, 1, 1])
 
-
-  # # Set 6: color 1 on top half, color 0 on bottom half
-  # for i in range(0,100):
-  #   for j in range(0, 50):
-  #       writer.writerow([0, 0, i, j, 1, 1])
-
-  # for i in range(0,100):
-  #   for j in range(50, 100):
-  #       writer.writerow([0, 0, i, j, 0, 0])
 
 with open("corner_color.txt",'w') as file:
   writer = csv.writer(file, delimiter = ",")
@@ -132,3 +101,28 @@ with open("checkerboard_weird.txt",'w') as file:
         for j in range((col+1)* 10, (col+2)* 10):
             writer.writerow([0, 0, i, j, 1, 1])
   
+
+with open("block_in_middle.txt",'w') as file:
+  writer = csv.writer(file, delimiter = ",")
+  writer.writerow(["ts", "user" ,"x_coordinate" ,"y_coordinate" ,"color", "label"])
+
+  for i in range(25,75):
+    for j in range(25, 75):
+        writer.writerow([0, 0, i, j, 1, 1])
+
+  for i in range(0,100):
+    for j in range(0, 25):
+        writer.writerow([0, 0, i, j, 0, 0])
+
+  for i in range(0,25):
+    for j in range(25, 75):
+        writer.writerow([0, 0, i, j, 0, 0])
+
+  for i in range(75,100):
+    for j in range(25, 75):
+        writer.writerow([0, 0, i, j, 0, 0])
+
+  for i in range(0,100):
+    for j in range(75, 100):
+        writer.writerow([0, 0, i, j, 0, 0])
+
